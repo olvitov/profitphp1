8271421dc618bf21bb5f9f17df5f0cdedd475a34
olvit <olvitov@gmail.com>
2016-07-10 16:59:26 +0300

    Калькулятор

---
+++ calk.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = $_POST['x'];
+
+$y = $_POST['y'];
+
+$res = $x + $y;
+
+echo $res;
--- index.php
+++ index.php
@@ -19,6 +19,18 @@ echo $months[$code];
 
 ?>
 
+
+<form action="/calk.php" method="post">
+<input type="number" name="x">
+    +
+    <input type="number" name="y">
+    <input type="submit" value="=">
+
+
+
+</form>
+
+
 <a href="http://profitphp1/index.php?code=jan">Link</a>
 <a href="http://profitphp1/index.php?code=feb">Link</a>
 
