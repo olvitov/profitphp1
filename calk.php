ae1fee25de7eb3054739b9a251365cd3511c8df4
olvit <olvitov@gmail.com>
2016-07-10 17:58:12 +0300

    Калькулятор Домашняя

--- calk.php
+++ calk.php
@@ -2,8 +2,24 @@
 
 $x = $_POST['x'];
 
+$act = $_POST['act'];
+
 $y = $_POST['y'];
 
-$res = $x + $y;
+// $res = $x + $y;
 
-echo $res;
+switch ($act) {
+
+    case  '+': echo $res = $x + $y;
+    break;
+
+    case  '-': echo $res = $x - $y;
+        break;
+
+    case  '*': echo $res = $x * $y;
+        break;
+
+    case  '/': echo $res = $x / $y;
+        break;
+
+}
--- index.php
+++ index.php
@@ -1,31 +1,56 @@
 <?php
 
+$x = $_POST['x'];
 
+$act = $_POST['act'];
 
-echo '<hr>';
-$code = $_GET['code'];
-$months = [
-    'jan' => Январь,
-    'feb' => Февраль,
-    'apr' => Март,
-    'mar' => Апрель,
+$y = $_POST['y'];
 
-];
+// $res = $x + $y;
+if ($act == '/' && 0 == $y) {
 
+    echo 'Деление на 0 не возможно';
+} else
+    switch ($act) {
 
+    case  '+':
+         $res = $x + $y;
+        break;
 
+    case  '-':
+         $res = $x - $y;
+        break;
 
-echo $months[$code];
+    case  '*':
+         $res = $x * $y;
+        break;
+
+    case  '/':
+         $res = $x / $y;
+        break;
+}
+
 
 ?>
 
 
-<form action="/calk.php" method="post">
-<input type="number" name="x">
-    +
-    <input type="number" name="y">
+<form action="/index.php" method="post">
+<input type="text" name="x" >
+ <select name="act">
+
+     <option>+</option>
+     <option>-</option>
+     <option>/</option>
+     <option>:</option>
+
+
+ </select>
+    <input type="text" name="y" value="<?php $_POST['y'] ?>">
     <input type="submit" value="=">
 
+<?php
+echo '<br />';
+echo $_POST['x'] . $_POST['act'] . $_POST['y'] . '=' . $res; ?>
 
 
 </form>
